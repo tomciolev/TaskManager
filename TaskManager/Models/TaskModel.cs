cdf7f9651da2f42ed55fcdf4496854649be991fc
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-04-05 23:08:17 +0200

    Gotowy projekt

--- TaskManager/Models/TaskModel.cs
+++ TaskManager/Models/TaskModel.cs
@@ -1,9 +1,17 @@
-﻿namespace TaskManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Models
 {
+    [Table("Tasks")]
     public class TaskModel
     {
+        [Key]
         public int TaskId { get; set; }
+        [Required(ErrorMessage = "Field \"Name\" is required.")]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(300)]
         public string Description { get; set; }
         public bool Done { get; set; }
     }
